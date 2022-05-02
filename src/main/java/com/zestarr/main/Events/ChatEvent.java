98ef9de09e48356197c925a1e09d2729d98cb8c4
Zestarr <zestarpersonal@gmail.com>
2022-05-02 16:16:22 -0400

    Added basic files

--- src/main/java/com/zestarr/main/Events/ChatEvent.java
+++ src/main/java/com/zestarr/main/Events/ChatEvent.java
@@ -1,6 +1,25 @@
 package com.zestarr.main.Events;
 
+import com.zestarr.main.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChatEvent implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        if (JavaPlugin.getPlugin(Main.class).getConfig().getBoolean("CustomChat")) {
+            try {
+                e.setFormat(ChatColor.translateAlternateColorCodes('&', JavaPlugin.getPlugin(Main.class).getConfig().getString("ChatFormat")));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        }
+    }
+
+
 }
--- src/main/java/com/zestarr/main/Main.java
+++ src/main/java/com/zestarr/main/Main.java
@@ -1,12 +1,22 @@
 package com.zestarr.main;
 
+import com.zestarr.main.Events.ChatEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("-------------------------" +  "\n\n" + "Superhero plugin has been enabled!" + "\n\n" + "-------------------------");
+        System.out.println("-------------------------" +  "\n\n" + ChatColor.RED + "Superhero plugin has been enabled!" + "\n\n" + "-------------------------");
+
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+
+        Bukkit.getPluginManager().registerEvents(new ChatEvent(), this);
+
+
 
     }
 
